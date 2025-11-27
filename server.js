be505b4521c43561d3c984c157c8a36f4e1b7f62
Bookyourneed <admin@bookyourneed.com>
2025-11-27 06:19:40 +0000

    aasasasa

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ app.use("/api/ridepayment", require("./routes/ridepayment"));
 app.use("/api/ride-booking", require("./routes/customerridebooking"));
 app.use("/api/notification", require("./routes/notification"));
 app.use("/api/worker/job", require("./routes/workerJobs"));
+app.use("/api", require("./routes/support"));
 app.use("/api/chat", require("./routes/chatUpload"));
 // ✅ Payment & Wallet Routes
 app.use("/api/payment", require("./routes/payment"));
